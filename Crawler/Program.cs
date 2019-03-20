fc32458542e0d911ba910fffcdf7d18a74ac3094
Douglas Mendes Barreto <mendes-barreto@live.com>
2019-03-20 13:46:42 -0400

    Add new project to git

---
+++ Crawler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
