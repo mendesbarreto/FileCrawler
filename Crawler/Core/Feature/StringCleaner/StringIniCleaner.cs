0b1ab5de4de2246bab50a5742175292e10b7b795
Douglas Mendes Barreto <mendes-barreto@live.com>
2019-03-25 11:56:00 -0400

    Replace string find to regex

--- Crawler/Core/Feature/StringCleaner/StringIniCleaner.cs
+++ Crawler/Core/Feature/StringCleaner/StringIniCleaner.cs
@@ -5,37 +5,21 @@ namespace FileCrawler.Core.Feature.StringCleaner
 {
     public class StringIniCleaner: IContentStringCleaner
     {
-        private Regex _replacementRegex = new Regex(@"['\&,\\/!@()]|\\s{2,}");
-        private Regex _replacementVariables = new Regex(@"^.*=");
-        private Regex _replacementParentheses = new Regex(@"\).*$");
+          //(A24130).*?,
+//        private Regex _replacementRegex = new Regex(@"[""'\&,\\/!@()""]|\\s{2,}");
+//        private Regex _replacementVariables = new Regex(@"^.*=");
+//        private Regex _replacementParentheses = new Regex(@"\).*$");
+//        private Regex _regexGet = new Regex(@"(A24130)(.+\w)\w|(A24130)(.+\w)(?=\()");
+//        private Regex _regexGet = new Regex(@"(A24130)(.+\w(?=[,]))|(A24130)(.+\w)(?=\()");
+//        private Regex _regexGet = new Regex(@"(A24130).*?(?=\()|(A24130).*?(?=[,]+)|(A24130).*?\w(?=[ ])|(A24130)(.+\w)");
+        private Regex _regexGet = new Regex(@"(A24130).*?(?=\()|(A24130).*?(?=[,]+)|(A24130).*?\w(?=[=> ])|(A24130)(.+\w)");
+//        private Regex _regexGet = new Regex(@"(A24130)(.+\w)(?=\()|(A24130)(.+\w)(?=[,]+)|(A24130)(.+\w)\w(?=[ ])|(A24130)(.+\w)");
+//        private Regex _regexGet = new Regex(@"(A24130)(.+\w)");
 
         public string clean(string value)
         {
-            var cleanedString = _replacementVariables.Replace(value, "");
-            cleanedString = _replacementParentheses.Replace(cleanedString, "");
-            cleanedString = _replacementRegex.Replace(cleanedString, "");
+            var cleanedString = _regexGet.Match(value).Value;
             return cleanedString;
-        }
-
-        private string RemoveStringParentheses(string value)
-        {
-            var splitedString = value.Split("(");
-            if( splitedString.Length > 1 && splitedString[0].Any() )
-            {
-                return splitedString[0];
-            }
-
-            return value;
-        }
-
-        private string RemovesVariableFromString(string value)
-        {
-            var splitedString = value.Split("=");
-            if(splitedString.Length > 1 && splitedString[1].Any() )
-            {
-                return splitedString[1];
-            }
-            return value;
         }
     }
 }
--- Crawler/Core/SearchPatternMatcher.cs
+++ Crawler/Core/SearchPatternMatcher.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace FileCrawler.Core
 {
